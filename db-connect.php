28b871c5386bdbade6124e070e37c976721c6996
alhassan <alhassanmr@gmail.com>
2017-12-04 17:43:14 +0000

    .

--- db-connect.php
+++ db-connect.php
@@ -5,7 +5,7 @@
    $user        = 'Developer';
    $pass        = 'developer';
 
-   // try to conncet to database
+   // try to connet to database
    $dbh = new PDO("mysql:dbname={$database};host={$host};port={3306}", $user, $pass);
 
    if(!$dbh){
